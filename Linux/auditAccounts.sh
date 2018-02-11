0f0854bc66d07323b6c1f530030c90000b3ac46d
Brandon Phillips <bmp0015@mix.wvu.edu>
2018-02-11 17:05:37 -0500

    Add the audit script

---
+++ Linux/auditAccounts.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+AUTHKEYFILE=`grep AuthorizedKeysFile /etc/ssh/sshd_config | awk '{print $2}' | awk -F'%h/' '{print $2}'`
+if [[ -z "$AUTHKEYFILE" ]];
+then
+    AUTHKEYFILE=`grep AuthorizedKeysFile /etc/ssh/sshd_config | awk '{print $2}'`
+fi
+
+for u in `cat /etc/passwd`
+do
+     `cat /etc/shells`
+     PASSWD="False"
+     HOMEDIR=`echo $u | awk -F':' '{ print $6}'`
+     USER=`echo $u | awk -F':' '{ print $1}'`
+     ENTRY=`echo $u | awk -F':' '{ print $7}' | awk '{$1=$1}1'`
+     if [[ ! -z "$ENTRY" && ! `echo $ENTRY | grep 'nologin'` && ! `echo $ENTRY | grep 'false'` ]];
+     then
+         if [[ ! `cat /etc/shadow | grep $USER | awk -F":" '($2 == "!" || $2 == "*") {print $1}'` ]];
+         then
+              PASSWD="True"
+         fi
+         if [[ -f "$HOMEDIR/$AUTHKEYFILE" ]];
+         then
+              KEY=`cat $HOMEDIR/$AUTHKEYFILE`
+         fi
+         echo -e "User: $USER\nShell: $ENTRY\nPassword: $PASSWD\nHome: $HOMEDIR\nKey: $KEY\n"
+     fi
+done
+
