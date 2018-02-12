d25593a01c012225846f6d502fab8452ac8356a0
Brandon Phillips <bmp0015@mix.wvu.edu>
2018-02-11 19:33:07 -0500

    dfasfas

--- Linux/accountChecker.sh
+++ Linux/accountChecker.sh
@@ -5,6 +5,6 @@ shadow="$(cat /etc/shadow)"
 
 while read -r line || [[ -n "$line" ]]
 do
-	echo -n "$line"
-	echo -e $shadow | grep "$(echo $line | cut ':' -f1)" | cut -d ':' -f 2-9
+	echo -n "$line:"
+	echo -e $shadow | grep "$(echo $line | cut -d ':' -f1)" | cut -d ':' -f 2-9
 done < /etc/passwd
